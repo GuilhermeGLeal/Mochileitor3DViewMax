90cb15c65a0e522a4ef005d3e9b267ee632de58d
Guilherme Gomes leal <guilhermegomesleal@hotmail.com>
2020-11-29 11:21:07 -0300

    ASAS Da liberda

--- Controladora/ControladoraArquivo.cs
+++ Controladora/ControladoraArquivo.cs
@@ -14,6 +14,7 @@ namespace Mochileitor3DView.Controladora
         public ControladoraArquivo(string path)
         {
             this.arqv = new ArquivoOBJ(path);
+            Console.log("asas");
         }
 
         public Ponto retornaPespectiva(Ponto pontAtual, int d)
